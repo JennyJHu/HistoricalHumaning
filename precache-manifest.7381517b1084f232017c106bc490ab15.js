491bf7197a42bd4f601fff5be22ba41ea46c492b
Jenny Hu <jenny-hu@Jennys-MacBook-Pro.local>
2024-01-14 21:12:50 +0000

    Updates

---
+++ precache-manifest.7381517b1084f232017c106bc490ab15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359fbd02303b06df8df3bc3ca72c4ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00508e013dc670ac1b98",
+    "url": "/static/css/main.f7db2cea.chunk.css"
+  },
+  {
+    "revision": "ca2b00aa72ab8c4223cd",
+    "url": "/static/js/2.2df51e1a.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "00508e013dc670ac1b98",
+    "url": "/static/js/main.fb7675e7.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  },
+  {
+    "revision": "cc6204268e51ba1438069d9d3a3210df",
+    "url": "/static/media/Director-Regular.cc620426.otf"
+  }
+]);
